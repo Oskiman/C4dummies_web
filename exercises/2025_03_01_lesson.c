20e14d1ee02e7576784d39dcc497b6e4b4064201
Paul <Oskiman@users.noreply.github.com>
2025-03-02 10:19:43 +0000

    Add files via upload

---
+++ exercises/2025_03_01_lesson.c
@@ -0,0 +1,34 @@
+// 2025_03_01_lesson.c
+// flip an array, reversing the order of it's elements
+// code from the c-for-dummies blog
+// posted 1 March 2025
+#include <stdio.h>
+
+int main()
+{
+	int a[] = {1, 2, 3, 4, 5, 6, 7, 8};
+	int size, x, save;
+
+	// obtain array size
+	size = sizeof(a) / sizeof(a[0]);
+
+	// output the array
+	for(x = 0; x < size; x++)
+		printf(" %d", a[x]);
+	putchar('\n');
+
+	// reverse the elements
+	for(x = 0; x < (size / 2); x++)
+	{
+		save = a[x];
+		a[x] = a[size - x - 1];
+		a[size - x - 1] = save;
+	}
+
+	// output the array
+	for(x = 0; x < size; x++)
+		printf(" %d", a[x]);
+	putchar('\n');
+
+	return 0;
+}

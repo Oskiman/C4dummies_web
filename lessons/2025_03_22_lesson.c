fd72fb443d451085dcd9c594f2cee01d5417b0c4
Paul <Oskiman@users.noreply.github.com>
2025-03-22 11:49:26 +0000

    Add files via upload

---
+++ lessons/2025_03_22_lesson.c
@@ -0,0 +1,26 @@
+// Code from: https://c-for-dummies.com/blog/?p=6878
+// prefix & postfix
+// 2 loops, each outputs values of x, initiliased to 0
+// The first uses prefix, the second uses postfix
+
+#include <stdio.h>
+
+int main()
+{
+	const int count = 10;
+	int x;
+
+	// prefixed
+	x = 0;
+	puts("Prefixed: ++x");
+	while(++x < count)
+		printf("%d\n", x);
+
+	// postfixed
+	puts("Postfixed: x++");
+	x = 0;
+	while(x++ < count)
+		printf("%d\n", x);
+
+	return 0;
+}

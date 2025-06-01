608ca9fe40485bcf2134a35b716bc3c494e6a0c9
Paul <Oskiman@users.noreply.github.com>
2025-06-01 09:48:14 +0100

    Add files via upload

---
+++ exercises/2025_06_01_exercise.c
@@ -0,0 +1,21 @@
+// C for Dummies exercise 2025_06_01
+// Write a program that translates values from base 10 to another base
+// Prompt for a base then for a value in decimal
+
+#include <stdio.h>
+
+int main(void)
+{
+	int base = 0;
+	int value = 0;
+
+	printf("Enter base (>1 <11): ");
+	scanf("%d", &base);
+
+	printf("Enter value (>1): ");
+	scanf("%d", &value);
+
+
+
+	return 0;
+}
